2eaec1d9ac3efbbaa1c960ba5a2ee82e11c39f94
Albin Warnicke <albin.warnicke@live.se>
2019-02-21 17:41:47 +0100

    Skapar en testfil för BT

---
+++ testBluepy.py
@@ -0,0 +1,1 @@
+# Testa Bluepy på raspberryn
---
+++ testBluetooth1.py
@@ -0,0 +1,36 @@
+# BtEchoClient.py
+
+from btcom import *  # TigerJython
+from btpycom import *  # Standard-Python (PC, Raspi, ...)
+
+
+def onStateChanged(state, msg):
+    global reply
+    if state == "CONNECTING":
+        print("Connecting"), msg
+    elif state == "CONNECTION_FAILED":
+        print("Connection failed"), msg
+    elif state == "CONNECTED":
+        print("Connected"), msg
+    elif state == "DISCONNECTED":
+        print("Disconnected"), msg
+    elif state == "MESSAGE":
+        print("Message"), msg
+        reply = msg
+
+
+serviceName = "EchoServer"
+print("Performing search for service name"), serviceName
+client = BTClient(stateChanged=onStateChanged)
+serverInfo = client.findService(serviceName, 20)
+if serverInfo == None:
+    print("Service search failed")
+else:
+    print("Got server info"), serverInfo
+    if client.connect(serverInfo, 20):
+        for n in range(0, 101):
+            client.sendMessage(str(n))
+            reply = ""
+            while reply == "":
+                time.sleep(0.001)
+        client.disconnect()
