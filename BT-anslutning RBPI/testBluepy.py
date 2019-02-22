dda916926f419d7516062cd88b1c84fb2b03a528
Albin Warnicke <albin.warnicke@live.se>
2019-02-22 18:19:40 +0100

    Update testBluepy.py

--- BT-anslutning RBPI/testBluepy.py
+++ BT-anslutning RBPI/testBluepy.py
@@ -1,1 +1,19 @@
 # Testa Bluepy på raspberryn
+import bleio
+
+# Create a Characteristic.
+chara = bleio.Characteristic(bleio.UUID(0x2919), read=True, notify=True)
+
+# Create a Service providing that one Characteristic.
+serv = bleio.Service(bleio.UUID(0x180f), [chara])
+
+# Create a peripheral and start it up.
+periph = bleio.Peripheral([service])
+periph.start_advertising()
+
+while not periph.connected:
+    # Wait for connection.
+    pass
+
+if periph.connected:
+    print("Connected!!!")
