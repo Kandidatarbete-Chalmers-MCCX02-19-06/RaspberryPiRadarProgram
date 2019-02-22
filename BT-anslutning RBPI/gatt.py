c50b41ebfab27f82efdba307e631c04b8da23d3d
Erik Angert Svensson <erik.angertsvensson@gmail.com>
2019-02-22 20:25:02 +0100

    test4

--- BT-anslutning RBPI/gatt.py
+++ BT-anslutning RBPI/gatt.py
@@ -1,31 +1,9 @@
 import gatt
 
-manager = gatt.DeviceManager(adapter_name='hci0')
+class AnyDeviceManager(gatt.DeviceManager):
+    def device_discovered(self, device):
+        print("Discovered [%s] %s" % (device.mac_address, device.alias()))
 
-class AnyDevice(gatt.Device):
-    def connect_succeeded(self):
-        super().connect_succeeded()
-        print("[%s] Connected" % (self.mac_address))
-
-    def connect_failed(self, error):
-        super().connect_failed(error)
-        print("[%s] Connection failed: %s" % (self.mac_address, str(error)))
-
-    def disconnect_succeeded(self):
-        super().disconnect_succeeded()
-        print("[%s] Disconnected" % (self.mac_address))
-
-    def services_resolved(self):
-        super().services_resolved()
-
-        print("[%s] Resolved services" % (self.mac_address))
-        for service in self.services:
-            print("[%s]  Service [%s]" % (self.mac_address, service.uuid))
-            for characteristic in service.characteristics:
-                print("[%s]    Characteristic [%s]" % (self.mac_address, characteristic.uuid))
-
-
-device = AnyDevice(mac_address='AA:BB:CC:DD:EE:FF', manager=manager)
-device.connect()
-
+manager = AnyDeviceManager(adapter_name='hci0')
+manager.start_discovery()
 manager.run()
