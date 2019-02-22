58f4ed3f7f11735ba0f44a2ef4a3b8275d2c2d7a
Erik Angert Svensson <erik.angertsvensson@gmail.com>
2019-02-22 20:01:48 +0100

    test2

--- BT-anslutning RBPI/gatt.py
+++ BT-anslutning RBPI/gatt.py
@@ -2,7 +2,7 @@ import gatt
 
 class AnyDeviceManager(gatt.DeviceManager):
     def device_discovered(self, device):
-        print("Discovered [%s] %s" % (device.mac_address, device.alias()))
+        print("[%s] Discovered, alias = %s" % (device.mac_address, device.alias()))
 
 manager = AnyDeviceManager(adapter_name='hci0')
 manager.start_discovery()
