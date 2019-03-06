2a79f59c8f8bf35e87eb9ddbcc07797be464e413
Emil <E.Johansson@zoho.com>
2019-03-06 15:44:59 +0100

    Definerat setup i egen metod bra/dåligt?

--- Python_mät/Matningar_Version_2.py
+++ Python_mät/Matningar_Version_2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import csv
 import scipy.io as sp
-import time
+import time as timer
 
 from acconeer_utils.clients.reg.client import RegClient
 from acconeer_utils.clients.json.client import JSONClient
@@ -18,24 +18,46 @@ def main():
     else:
         port = args.serial_port or example_utils.autodetect_serial_port()
         client = RegClient(port)
-
     client.squeeze = False
-    # Setup parameters
-    config = configs.IQServiceConfig()
-    config.sensor = args.sensors
-    config.range_interval = [0.20, 0.50]
-    config.sweep_rate = 100
-    config.gain = 1
-    tid = 10
-    sekvenser = config.sweep_rate * tid
-    config.running_average_factor = 0.5
 
+    # Setup parameters
+    filename = "Manniska_lang_0305_Test2.csv"
+    config = setup_parameters()
+    time = 10
+    seq = config.sweep_rate * time
+    config.sensor = args.sensors
     info = client.setup_session(config)
     num_points = info["data_length"]
-    print(num_points)
+    # print(num_points)
+    info_file(filename, config, num_points, time, seq)  # Setup info file
 
-    filename = "Ton20_Sweep100_Test6.csv"
+    matris = np.zeros((seq, num_points), dtype=np.csingle)
+    client.start_streaming()
+    print("Starting...")
 
+    start = timer.time()
+    for i in range(0, seq):
+        info, sweep = client.get_next()
+        matris[0][:] = sweep[:]
+        matris = np.roll(matris, 1, axis=0)
+    end = timer.time()
+    print(i+1)
+    print((end - start), "s")
+    # print("Disconnecting...")
+    #np.savetxt(filename, matris, delimiter=";")
+    client.disconnect()
+
+
+def setup_parameters():
+    config = configs.IQServiceConfig()
+    config.range_interval = [0.5, 0.7]
+    config.sweep_rate = 25
+    config.gain = 1
+    #config.running_average_factor = 0.5
+    return config
+
+
+def info_file(filename, config, num_points, time, seq):
     with open("Info_"+filename, "w", newline='\n') as result:
         writer = csv.writer(result, delimiter=';')
         writer.writerow(["Gain: ", str(config.gain)])
@@ -43,30 +65,9 @@ def main():
         writer.writerow(["Max range:", str(config.range_interval[1])])
         writer.writerow(["Data length:", str(num_points)])
         writer.writerow(["Freq: ", str(config.sweep_rate)])
-        writer.writerow(["Tejp: ", "Människa"])
-        writer.writerow(["Tid: ", tid])
-        writer.writerow(["Sekvenser: ", sekvenser])
-
-    matris = np.zeros((sekvenser, num_points), dtype=np.csingle)
-    client.start_streaming()
-    print("Starting...")
-    start = time.time()
-    for i in range(0, sekvenser):
-        matris = np.roll(matris, 1, axis=0)
-        info, sweep = client.get_next()
-        matris[0][:] = sweep[:]
-    end = time.time()
-    print(i+1)
-    print((end - start), "s")
-    # print("Disconnecting...")
-    np.savetxt(filename, matris, delimiter=";")
-    # with open("IQ2_Test.csv", "w", newline='') as f:
-    #    writer = csv.writer(f)
-    #    writer.writerows(matris)
-    #np.savetxt('outfile.csv', matris.view(float), delimiter=";")
-
-    client.disconnect()
-    # print(matris)
+        writer.writerow(["Tejp: ", "Människa. Puls referens 50-60 BPM"])
+        writer.writerow(["Tid: ", time])
+        writer.writerow(["Sekvenser: ", seq])
 
 
 if __name__ == "__main__":
--- Python_mät/annotate.py
+++ Python_mät/annotate.py
@@ -17,20 +17,17 @@ def main():
         port = args.serial_port or example_utils.autodetect_serial_port()
         client = RegClient(port)
 
-    config = configs.EnvelopeServiceConfig()
+    config = config_setup()
     config.sensor = args.sensors
-    config.range_interval = [0.2, 0.6]
-    config.sweep_rate = 10
-    config.gain = 0.6
+
     tid = 10
     sekvenser = tid * config.sweep_rate
-    filename = "Lutande_Reflektor_jacka.csv"
+    filename = "Reflektor_2.csv"
 
     info = client.setup_session(config)
     num_points = info["data_length"]
-    # print(info)
 
-    amplitude_y_max = 15000
+    amplitude_y_max = 22000
 
     fig, (amplitude_ax) = plt.subplots(1)
     fig.set_size_inches(12, 6)
@@ -39,9 +36,9 @@ def main():
     for ax in [amplitude_ax]:
         ax.set_xlabel("Depth (m)")
         ax.set_xlim(config.range_interval)
-        ax.set_xticks(np.linspace(0.2, 0.6, num=5))
-        ax.set_xticks(np.concatenate([np.linspace(0.21, 0.29, num=9), np.linspace(
-            0.31, 0.39, num=9), np.linspace(0.41, 0.49, num=9), np.linspace(0.51, 0.59, num=9)]), minor=True)
+        ax.set_xticks(np.linspace(0.4, 0.8, num=5))
+        ax.set_xticks(np.concatenate([np.linspace(0.41, 0.49, num=9), np.linspace(
+            0.51, 0.59, num=9), np.linspace(0.61, 0.69, num=9), np.linspace(0.71, 0.79, num=9)]), minor=True)
         ax.grid(True, which='major')
         ax.grid(True, which='minor')
 
@@ -54,8 +51,6 @@ def main():
     fig.tight_layout()
     plt.ion()
     plt.show()
-    xmax = 0
-    ymax = 0
 
     interrupt_handler = example_utils.ExampleInterruptHandler()
     print("Press Ctrl-C to end session")
@@ -63,38 +58,43 @@ def main():
     client.start_streaming()
     matris = np.zeros((sekvenser, 2))
 
-    # while not interrupt_handler.got_signal:
-    for i in range(0, sekvenser):
+    while not interrupt_handler.got_signal:
+        # for i in range(0, sekvenser):
         info, sweep = client.get_next()
         amplitude = np.abs(sweep)
         ymax = amplitude.max()
-        matris = np.roll(matris, 1, axis=0)
-        matris[0][:] = [xmax, ymax]
-
-        #xmax = num_points[np.argmax(amplitude)]
         xmax = config.range_interval[0] + (config.range_interval[1] - config.range_interval[0]) * \
             (np.argmax(amplitude)/num_points)
+        matris[0][:] = [xmax, ymax]
+        matris = np.roll(matris, 1, axis=0)
+
         text = "x={:.2f}, y={:.2f}".format(xmax, ymax)
         bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
         arrowprops = dict(arrowstyle="->", connectionstyle="angle,angleA=0,angleB=90")
         kw = dict(xycoords='data', textcoords="axes fraction",
                   arrowprops=arrowprops, bbox=bbox_props, ha="right", va="top")
         annotate = ax.annotate(text, xy=(xmax, ymax), xytext=(0.96, 0.96), **kw)
-        #annotate = plt.annotate(text, xy=(0.2, 5000), xytext=(0.3, 8000))
 
         amplitude_line.set_ydata(amplitude)
 
         if not plt.fignum_exists(1):  # Simple way to check if plot is closed
             break
-        # print(xmax)
         fig.canvas.flush_events()
         annotate.remove()
-    matris = np.mean(matris, axis=0)
-    np.savetxt(filename, matris, delimiter=",")
+    # matris = np.mean(matris, axis=0)
+    # np.savetxt(filename, matris, delimiter=",")
 
     print("Disconnecting...")
     plt.close()
     client.disconnect()
+
+
+def config_setup():
+    config = configs.EnvelopeServiceConfig()
+    config.range_interval = [0.4, 0.8]
+    config.sweep_rate = 2
+    config.gain = 1
+    return config
 
 
 if __name__ == "__main__":
