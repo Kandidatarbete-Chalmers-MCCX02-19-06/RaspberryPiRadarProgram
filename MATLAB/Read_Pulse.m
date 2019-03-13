b0f067876c34836416deb8ed0595e16c7607b7ee
simonnordenstrom <47887729+simonnordenstrom@users.noreply.github.com>
2019-03-13 21:37:30 +0100

    Read_Pulse-kommentarer

--- MATLAB/Read_Pulse.m
+++ MATLAB/Read_Pulse.m
@@ -17,13 +17,13 @@ if(size(D,2)~=size(E,2))
     return
 end
 
-AvHR = str2num(C(D(1)+7:E(1)-1)) %Average HR first value in tcx-file
-MaxHR = str2num(C(D(2)+7:E(2)-1)) %Max HR second value in tcx-file
+AvHR = str2num(C(D(1)+7:E(1)-1)) %Average HR is first value after <Value> in tcx-file.  
+MaxHR = str2num(C(D(2)+7:E(2)-1)) %Max HR is second value in tcx-file
 
 
 %Pulse = zeros(size(D),1);
 for i = 3:size(D,2)
-    HR(i-2) = str2num(C(D(i)+7:E(i)-1))
+    HR(i-2) = str2num(C(D(i)+7:E(i)-1)) %i-2 because the 1st HR value is after the 3rd <Value> in tcx
 end
 
 tHR = 0:size(HR,2)-1
