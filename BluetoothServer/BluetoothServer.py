0aae715bdfc16e2bd484eb12b8b85011c530cee6
Erik Angert Svensson <erik.angertsvensson@gmail.com>
2019-03-22 19:08:26 +0100

    Random value function added

--- BluetoothServer/BluetoothServer.py
+++ BluetoothServer/BluetoothServer.py
@@ -1,14 +1,26 @@
 # Importing the Bluetooth Socket library
+from typing import Any, Union
+
 import bluetooth
 import threading
 import time
+import math
+import random
 
-clientList = []
-addressList = []
-readThreadList = []
+clientList = []         # list for each connected device, sockets
+addressList = []        # list for mac-adresses from each connected device
+readThreadList = []     # list for threads to recieve from each device
+sinvalue = 0
 
 host = ""
 port = 1  # Raspberry Pi uses port 1 for Bluetooth Communication
+
+def addData(i):
+    data = math.sin(i)
+    noise = random.random()
+    data += (noise - 0.5)
+    return data
+
 # Creaitng Socket Bluetooth RFCOMM communication
 server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 print('Bluetooth Socket Created')
@@ -18,7 +30,8 @@ try:
 except:
     print("Bluetooth Binding Failed")
 
-class ConnectDevicesThread(threading.Thread):
+
+class ConnectDevicesThread(threading.Thread):       # thread for listening to and accepting incoming connections
     def __init__(self,):
         super(ConnectDevicesThread, self).__init__()
         server.listen(7)
@@ -30,9 +43,10 @@ class ConnectDevicesThread(threading.Thread):
             addressList.append(a)
             readThreadList.append(ReadDeviceThread(c))       # one thread for each connected device
             readThreadList[len(readThreadList)-1].start()
-            #print("Client:", c)
+            # print("Client:", c)
 
-class ReadDeviceThread(threading.Thread):
+
+class ReadDeviceThread(threading.Thread):       # thread for reading messages from data
     client = None
     def __init__(self, client):
         self.client = client
@@ -41,10 +55,11 @@ class ReadDeviceThread(threading.Thread):
     def run(self):
         try:
             while True:
-                data = self.client.recv(1024)
+                data = self.client.recv(1024)       # important to write self.client everywhere in the class/thread
                 print(data.decode('utf-8'))
         except:
             pass
+
 
 connectDevices = ConnectDevicesThread()
 connectDevices.start()
@@ -53,7 +68,8 @@ for i in range(1,100):
     time.sleep(1)
     while len(clientList) == 0:
         pass
-    write = 'String from Raspberry Pi after received message' + str(i)
+    #write = 'String from Raspberry Pi after received message' + str(i)
+    write = addData(sinvalue)
     print(write)
     # print(write.encode('utf-8'))
     for client in clientList:
@@ -66,5 +82,6 @@ for i in range(1,100):
             client.close()
             print('remove : ' + str(addressList[clientList.index(client)]))
             clientList.remove(client)
+    sinvalue += 0.157
 
 server.close()
