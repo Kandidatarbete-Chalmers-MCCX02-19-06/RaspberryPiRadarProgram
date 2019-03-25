ac356fbcc95e4c4ad44b81975821ca2d241057cc
simonnordenstrom <47887729+simonnordenstrom@users.noreply.github.com>
2019-03-25 21:44:46 +0100

    Bara testar lite

--- MATLAB/mains/IQ_txt_read_main_simon.m
+++ MATLAB/mains/IQ_txt_read_main_simon.m
@@ -1,12 +1,22 @@
 addpath(genpath(pwd))
+addpath('Dataread')
+addpath('tracker')
 clc
 clear all
 format shorteng
-addpath("IQ Read")
-addpath("Target tracking")
 
-filename = 'Manniska_Sweep100_Test3.csv'
-[dist,amp, phase,t,S,D,A,P, gain, L_start, L_end, L_data, L_seq, Fs] = IQ_read_3(filename);
+%Reading of Radar data
+%filename_radar = 'Manniska_1m_0305_Test1.csv'
+filename_radar = 'Manniska_lang_0305_Test2.csv'
+%filename_radar = 'PulsMetern_0313_Test1.csv'
+filename_radar = 'SimonPremiar_0314_Test2.csv'
+[dist,amp, phase,t,gain, L_start, L_end, L_data, L_seq, Fs] = IQ_read_3(filename_radar);
+
+%Reading of wristband pulse measurements
+%filename_pulsemeter = 'PulsMetern_0313_Test1.tcx'
+filename_pulsemeter = 'SimonPremiar_0314_Test2.tcx'
+[AvHR, MaxHR, HR, tHR] = Read_Pulse(filename_pulsemeter,false);
+
 gain = gain
 L_start = L_start
 L_end = L_end
@@ -17,11 +27,30 @@ c = 3e8;%[m/s]
 fc = 60.5e9;% [Hz]
 wavelength = c/fc
 
-%%
+
+%test of matched filter
+MF = amp(1,:).*(cos(phase(1,:)) + 1i*sin(phase(1,:)) );%get the first vector for matched filter
+MF = flip(MF);
+Emf = sum(abs(MF).^2);
+%conv(data,filter,SHAPE,'same');
+
+% for i = 1:L_seq
+%     filtered_refl(i,:) = filter(MF,Emf, (amp(i,:).*(cos(phase(i,:)) + 1i*sin(phase(i,:)) ) ));
+%     i = i + 1;
+% end
+
+%amp = abs(filtered_refl);
+%phase = angle(filtered_refl);
+
+[T,D,A,P] = SURF_PREP(dist,amp, phase,t);
+
+
 %Detektering och följning utav mål
 start_distance = 0.37%m
+start_distance = 0.4%m
 N_avg = 10;
 [t,target_amplitude, target_phase, target_distance] = target_tracker_2(t,dist,amp,phase,start_distance,N_avg);
+
 
 
 figure(1)
@@ -38,34 +67,17 @@ xlabel('Distance [m]')
 
 %surf test
 figure(2)
-surf(S,D,A)
+surf(T,D,A)
 ylabel('Distance [m]')
 xlabel('Time [s]')
 zlabel('Reflection amplitude')
 
 figure(3)
-surf(S,D,P)
+surf(T,D,P)
 ylabel('Distance [m]')
 xlabel('Time [s]')
 zlabel('Reflection phase [rad]')
 
-
-% index_test = 36% i %
-% index_test = round(index_test/100*L_data);
-% P = phase(:,index_test);
-% A = amp(:,index_test);
-% 
-% 
-% figure(4)
-% subplot(1,2,1)
-% plot(t,A)
-% xlabel('Time [s]')
-% ylabel('Reflection Amplitude')
-% 
-% subplot(1,2,2)
-% plot(t,P)
-% xlabel('Time [s]')
-% ylabel('Reflection phase [rad]')
 
 %unwrap test
 target_phase = unwrap(target_phase);
@@ -73,7 +85,7 @@ target_phase = unwrap(target_phase);
 
 %Signal filtreringstest
 disp('Downsampling with ratio r:')
-r = 5
+r = 1
 %Down sampling
 target_amplitude = decimate(target_amplitude,r);
 target_phase = decimate(target_phase,r);
@@ -86,46 +98,157 @@ Fs = Fs/r%New sample rate in time domain
 %Delta distance of tracked target
 target_delta_distance = wavelength/2/pi/2*target_phase;
 
-%Windowing
-W = window(@flattopwin,1,L_seq)';
-target_amplitude = target_amplitude .* W;
-target_phase = target_phase .* W;
-target_distance = target_distance .* W;
+%filer data into two bandwidths
+F_low_BR = 0.2
+F_high_BR = 0.7
 
-%Padded FFT
-FFT_resolution = 0.001%[Hz resolution]
-L_fft = max(Fs/FFT_resolution,L_seq)%needed length of FFT, or orignial.
-f = Fs*(0:(L_fft/2))/L_fft;
+F_low_HR = 0.8
+F_high_HR = 6
+BWrel_transband_BR = 0.5
+BWrel_transband_HR = 0.2
+Atten_stopband = 60 %(!)
+%Atten_stopband = 60
+[delta_distance_BR] = bandpassfilter(target_delta_distance,Fs,F_low_BR,F_high_BR,BWrel_transband_BR,Atten_stopband);
+[delta_distance_HR] = bandpassfilter(target_delta_distance,Fs,F_low_HR,F_high_HR,BWrel_transband_HR,Atten_stopband);
 
-Y = fft(target_delta_distance - mean(target_delta_distance),L_fft);
-P2 = abs(Y/L_fft);
-P1 = P2(1:L_fft/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-target_delta_distance_fft = P1;%FFT for delta distance from phase of target
+%splitting data
+% T_start = 50
+% T_end = 65
+% i_start = round(T_start*Fs);
+% i_stop = round(T_end*Fs);
+% t = t(i_start:i_stop);
+% L_seq = length(i_start:i_stop)
+% delta_distance_HR = delta_distance_HR(i_start:i_stop);
+% delta_distance_BR = delta_distance_BR(i_start:i_stop);
 
-Y = fft(target_amplitude-mean(target_amplitude),L_fft);
-P2 = abs(Y/L_fft);
-P1 = P2(1:L_fft/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-target_amplitude_fft = P1;%FFT of target reflection amplitude
+%FFTs
+%F_resolution = 0.01 %[Hz]
+F_resolution = 1/60 %[Hz]
+[f_BR,delta_distance_BR_FFT] = smartFFT_abs(delta_distance_BR,Fs,F_resolution);
+[f_HR,delta_distance_HR_FFT] = smartFFT_abs(delta_distance_HR,Fs,F_resolution);
 
+%delta_distance_HR_FFT = movmean(delta_distance_HR_FFT,25);
 
+%Frequency finding
+BW_comb = 2/60 %[Hz] Tightness of tone scanning
+BW_comb = 3/60
+N_harmonics = 3%number of harmonics to look at, incl fundamental
+%Scan span for breating rate
+%Fscan_lower_BR = F_low_BR
+%Fscan_upper_BR = F_low_BR/3
 
+%Scan span for heartrate
+Fscan_lower_HR = 50/60
+Fscan_upper_HR = 150/60
+%f_detected_BR = basetone_finder(f_BR,delta_distance_BR_FFT,Fs,F_low_BR,F_high_BR,BW_comb)
+[f_search,P_sum_N,f_fine] = basetone_finder(f_HR,delta_distance_HR_FFT,Fs,Fscan_lower_HR,Fscan_upper_HR,BW_comb,N_harmonics,true);
+f_detected_HR = f_fine
+BPM_HR = 60*f_detected_HR
 
-
+%Plots
 figure(5)
-subplot(1,2,1)
-loglog(f,target_amplitude_fft)
-ylabel('Amplitude of tones in reflection amplitude [arb]')
+subplot(2,2,1)
+plot(f_BR,delta_distance_BR_FFT)
+ylabel('FFT of HR spectrum [m]')
 xlabel('f [Hz]')
+xlim([F_low_BR F_high_BR])
 
-subplot(1,2,2)
-loglog(f,target_delta_distance_fft)
-ylabel('Amplitudes of tones in reflection phase [m]')
+subplot(2,2,2)
+plot(f_HR,delta_distance_HR_FFT)
+ylabel('FFT of HR spectrum [m]')
 xlabel('f [Hz]')
+xlim([F_low_HR F_high_HR])
 
+
+%time 
+subplot(2,2,3)
+plot(t,delta_distance_BR)
+ylabel('Plot of BR bandwidth [m]')
+xlabel('Time [s]')
+
+subplot(2,2,4)
+plot(t,delta_distance_HR)
+ylabel('Plot of HR bandwidth [m]')
+xlabel('Time [s]')
+
+
+%Waterfall FFT plot of HR
 
 figure(6)
+T_resolution = 30 % Time resolution[s]
+%F_resolution = 1/60*1.5
+overlap = 50% overlap of slidiing frames [%]
+S_leakage = 1 % Leakage from tones 
+
+% [P,F,T] = pspectrum(delta_distance_HR,Fs,'spectrogram', ...
+%     'FrequencyResolution',F_resolution,'Overlap',overlap,'Leakage',S_leakage);
+
+[P,F,T] = pspectrum(delta_distance_HR,Fs,'spectrogram', ...
+     'TimeResolution',T_resolution,'Overlap',overlap,'Leakage',S_leakage);
+
+
+pcolor(T,F,log10(P));
+%pcolor(P)
+colorbar
+xlabel('Time [s]')
+ylabel('Frequency [Hz]')
+ylim([0 6])
+F_resolution = F_resolution
+T_resolution = T(2)-T(1)
+BW_comb = F_resolution;
+
+%test with overtone finder
+%ADD sliding window data
+[f_search,P_sum_N,f_fine] = basetone_finder(F,P(:,1),Fs,Fscan_lower_HR,Fscan_upper_HR,BW_comb,N_harmonics,false);
+f_found = f_fine;
+P_N = zeros(length(T),length(f_search));
+P_N(1,:) = P_sum_N;
+BPM_search = f_search'*60;
+for i = 2:length(T)
+    %Finds fundamental for all time slots..
+    [f_search,P_sum_N,f_fine] = basetone_finder(F,P(:,i),Fs,Fscan_lower_HR,Fscan_upper_HR,BW_comb,N_harmonics,false);
+    P_N(i,:) = P_sum_N;
+    f_found(i) = f_fine;
+end
+
+
+
+figure(8)
+pcolor(T,BPM_search,P_N');
+%pcolor(P)
+colorbar
+xlabel('Time [s]')
+ylabel('Frequency [Bpm]')
+ylim([40 100])
+%test
+hold on
+plot(tHR,HR,'r','LineWidth',2)
+
+f_found = f_found';
+size(T)
+size(f_found)
+title('Probability map of radar data vs excerice watch')
+
+%plot of BPM over time
+%Kolla errorbaren om de skall vara halva
+E = ones(size(T)).*F_resolution.*60;
+figure(9)
+errorbar(T,f_found*60,E,'-s','MarkerSize',10,...
+    'MarkerEdgeColor','red','MarkerFaceColor','red')
+hold on
+plot(tHR,HR)
+plot([min(tHR) max(tHR)],[AvHR AvHR],'--r')
+legend('Radar','Pulse watch','Average from pulse watch')
+xlabel('Time [s]')
+ylabel('Frequency [Bpm]')
+ylim(60*[Fscan_lower_HR Fscan_upper_HR])
+
+
+
+
+%Tracked data
+
+figure(10)
 subplot(2,2,1)
 plot(t,target_amplitude)
 xlabel('t [s]')
@@ -145,13 +268,6 @@ subplot(2,2,4)
 plot(t,5e-3/2/pi/2*(target_phase - mean(target_phase)))
 xlabel('t [s]')
 ylabel('Delta distance of tracked target [m]')
-
-
-%Plot of IQ data
-figure(7)
-I = target_amplitude(1:L_seq/2).*cos(target_phase(1:L_seq/2));
-Q = target_amplitude(1:L_seq/2).*sin(target_phase(1:L_seq/2));
-plot(I,Q)
 
 
 
