81c34135f7676d1fd49c1b4cbd63cf4dfc280b66
Emil <E.Johansson@zoho.com>
2019-03-28 18:12:27 +0100

    m

--- Main program/bluetooth_app.py
+++ Main program/bluetooth_app.py
@@ -49,8 +49,10 @@ class bluetooth_app:
             self.client_list.append(c)
             self.address_list.append(a)
             # one thread for each connected device
-            self.read_thread_list.append(threading.Thread(target=self.read_device, args=(c)))
-            self.read_thread_list[-1].start()
+            self.read_thread_list.append(c, a)
+            self.thread = threading.Thread(target=self.read_device, args=(c))
+            # self.read_thread_list.append(threading.Thread(target=self.read_device, args=(c)))
+            # self.read_thread_list[-1].start()
             print("New client: ", a)
 
     def read_device(self, client):
