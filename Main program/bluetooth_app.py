39fe9efb4087cbdacc2949fedf26aecee4cdda89
Emil <E.Johansson@zoho.com>
2019-03-29 19:48:45 +0100

    m

--- Main program/bluetooth_app.py
+++ Main program/bluetooth_app.py
@@ -69,15 +69,16 @@ class bluetooth_app:
         c = self.client_list[-1]
         try:
             while self.run:
-                byte, address = c.recvfrom(1024)
-                print(str(byte))
+                data = c.recvmsg(1024)
+                print(data.decode('utf-8'))
+                print(str(data))
+                p = 'poweroff'
                 try:
-                    print(type(byte.decode('utf-8'))
-                    print(type('poweroff'))
-                    if byte.decode('unicode') == 'poweroff':
+                    # print(type(data.decode('utf-8'))
+                    if data.decode('utf-8') == p.encode('utf-8'):
                         print("Shutdown starting")
                         # TODO Erik: Power off python program and Raspberry Pi
-                        self.run=False
+                        self.run = False
                         for client in self.client_list:     # closes and removes clients from list to cause exceptions and thereby closing the thread
                             client.close()
                             print('remove client: ' +
@@ -94,15 +95,15 @@ class bluetooth_app:
     def write_data_to_app(self, data, data_type):
         # print(data + ' ' + data_type)
         if data_type == 'heart rate':
-            string=' HR ' + data + ' '
+            string = ' HR ' + data + ' '
             # print(string)
             self.send_data(string)
         elif data_type == 'breath rate':
-            string=' BR ' + data + ' '
+            string = ' BR ' + data + ' '
             # print(string)
             self.send_data(string)
         elif data_type == 'real time breath':
-            string=' RTB ' + data + ' '
+            string = ' RTB ' + data + ' '
             self.send_data(string)
 
     def send_data(self, write):
@@ -116,13 +117,13 @@ class bluetooth_app:
                 print("Error")
 
     def add_data(self, i):
-        data=[70 + math.sin(i), 20 + math.sin(i+math.pi/4)]
-        noise=random.random()
+        data = [70 + math.sin(i), 20 + math.sin(i+math.pi/4)]
+        noise = random.random()
         data[0] += 5*(noise - 0.5)
-        noise=random.random()
+        noise = random.random()
         data[1] += noise
-        data[0]=round(data[0])
-        data[1]=round(data[1])
+        data[0] = round(data[0])
+        data[1] = round(data[1])
         return str(data[0]) + ' ' + str(data[1])
 
     def get_data_from_queue(self):
