da7ea34ecc95a62c860eee3edc781a42fb9ede4c
Emil <E.Johansson@zoho.com>
2019-04-01 08:53:59 +0200

    m

--- Main program/bluetooth_app.py
+++ Main program/bluetooth_app.py
@@ -84,7 +84,7 @@ class bluetooth_app:
                 # if data.decode('utf-8') == 'poweroff':
                 if time.time() > self.timeout:
                     print("Shutdown starting")
-                    subprocess.call(["sudo", "shutdown", "-h", "now"])
+                    #subprocess.call(["sudo", "shutdown", "-h", "now"])
                     # TODO Erik: Power off python program and Raspberry Pi
                     try:
                         self.run = False
