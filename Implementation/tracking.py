3487e5975ea86b5ffd4b1bbee1900dc75cfb8996
Emil <E.Johansson@zoho.com>
2019-04-02 17:30:03 +0200

    m

--- Implementation/tracking.py
+++ Implementation/tracking.py
@@ -5,31 +5,45 @@ import scipy as sp
 
 # data is stored in self.data
 def tracking(self):
+    counter = 0  # Used only for if statement only for first iteration and not when data_idx goes back to zero
+    N_avg = 10
     self.data_matrix[self.data_idx][:] = self.data
     dist = self.num_points     # number of datapoints in data # self.num_points
     interval = self.config.range_interval[1] - self.config.range_interval[0]       # maximum value
     self.start_distance = 0.37
     # choosen index closest to starting distance
-    I = round(((self.start_distance - self.config.range_interval[0]) / interval) * dist)
+    I = np.round(((self.start_distance - self.config.range_interval[0]) / interval) * dist)
 
-    if self.data_idx == 0:      # things that only happens first time
+    if self.data_idx == 0 and counter == 0:      # things that only happens first time
         self.I_peaks[0] = I
         self.locks = sp.signal.find_peaks(np.abs(self.data))
-        self.I_peaks[0] = np.amin(np.abs(self.locks - self.I_peaks[0]))  # Maybe one more Locs
+        I = np.amin(np.abs(self.locks - self.I_peaks[0]))
+        self.I_peaks[0] = self.locks[I]
         self.I_peaks_filtered[0] = self.I_peaks[0]
-        self.tracked_distance = self.I_peaks_filtered[0] / dist * interval
-        self.tracked_amplitude = np.abs(self.data(self.I_peaks_filtered[0]))
-        self.tracked_phase = np.angle(self.data(self.I_peaks_filtered[0]))
+        self.tracked_distance[0] = self.I_peaks_filtered[0] / dist * interval
+        self.tracked_amplitude[0] = np.abs(self.data(self.I_peaks_filtered[0]))
+        self.tracked_phase[0] = np.angle(self.data(self.I_peaks_filtered[0]))
 
-     # After first seq continous tracking
-     elif:
-        self.locks = None     
+    # After first seq continous tracking
+    else:
+        self.locks = None
         self.locks = sp.signal.find_peaks(np.abs(self.data))
-        I = np.amin(self.locks - self.I_peaks_filtered[self.data_idx -1])
+        I = np.amin(self.locks - self.I_peaks_filtered[self.data_idx - 1])
         if self.locks == None:
-                self.I_peaks[self.data_idx] = self.I_peaks[self.data_idx-1]
-        self.I_peaks[self.data_idx] = 
+            self.I_peaks[self.data_idx] = self.I_peaks[self.data_idx-1]
+        else:
+            self.I_peaks[self.data_idx] = self.locks[I]
+        if counter == 0:
+            self.i_avg_start = np.amax([1, self.data_idx - N_avg])
+        else:
+            self.i_avg_start = self.data_idx - N_avg
+            counter = 1
 
+        self.I_peaks_filtered[self.data_idx] = np.round(np.mean(self.I_peaks(self.i_avg_start: self.data_idx)))
+        self.tracked_distance[self.data_idx] = self.I_peaks_filtered[self.data_idx] / dist * interval
 
-
-     
+        self.tracked_amplitude[self.data_idx] = np.abs(
+            self.data(self.I_peaks_filtered[self.data_idx]))
+        self.data(self.I_peaks_filtered[self.data_idx])
+        self.tracked_phase[self.data_idx] = np.angle(
+            self.data(self.I_peaks_filtered[self.data_idx]))
