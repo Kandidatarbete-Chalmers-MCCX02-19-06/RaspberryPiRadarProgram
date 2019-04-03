24689291dac7dd466974bb12400ca63e92bc2963
Emil <E.Johansson@zoho.com>
2019-04-03 14:41:49 +0200

    m

--- Implementation/tracking_test.py
+++ Implementation/tracking_test.py
@@ -221,10 +221,10 @@ class Tracking:
                 self.i_avg_start = self.data_idx - N_avg
                 counter = 1
 
-            print(self.i_avg_start)
             self.I_peaks_filtered[0][self.data_idx] = np.round(
                 np.mean(self.I_peaks[0][self.i_avg_start:self.data_idx]))
             print(self.I_peaks_filtered)
+            print(self.i_avg_start)
             self.tracked_distance[0][self.data_idx] = self.I_peaks_filtered[0][self.data_idx] / dist * interval
 
             self.tracked_amplitude[0][self.data_idx] = np.abs(
