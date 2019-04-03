ce3e5593907c4a5c6d83ebe04466c87d593a30ef
Emil <E.Johansson@zoho.com>
2019-04-03 16:52:13 +0200

    m

--- Implementation/tracking_test.py
+++ Implementation/tracking_test.py
@@ -178,7 +178,7 @@ class Tracking:
             self.tracked_phase[0][self.data_idx] = np.angle(
                 self.data[int(self.I_peaks_filtered[0][self.data_idx])])
 
-        return self.tracked_distance
+        return self.peaks
 
 
 if __name__ == "__main__":
