824f5fb0f25a01f6a62f9ef4f62f49aa365a254d
simonnordenstrom <47887729+simonnordenstrom@users.noreply.github.com>
2019-04-03 09:52:58 +0200

    Added plots!!

--- Implementation/tracking.py
+++ Implementation/tracking.py
@@ -7,17 +7,20 @@ import matplotlib.pyplot as plt
 # data is stored in self.data
 def tracking(self):
     counter = 0  # Used only for if statement only for first iteration and not when data_idx goes back to zero
-    N_avg = 10
+    N_avg = 10  # Number of total peaks to average over
+    self.start_distance = 0.37  # Initial guess for where
     self.data_matrix[self.data_idx][:] = self.data
     dist = self.num_points     # number of datapoints in data # self.num_points
     # maximum value
     interval = self.config.range_interval[1] - self.config.range_interval[0]
-    self.start_distance = 0.37
-    # choosen index closest to starting distance
-    I = np.round(
-        ((self.start_distance - self.config.range_interval[0]) / interval) * dist)
 
     if self.data_idx == 0 and counter == 0:      # things that only happens first time
+        # chooses index closest to starting distance
+        I = np.round(
+            ((self.start_distance - self.config.range_interval[0]) / interval) * dist)
+
+        #I = np.abs(self.data).index(sp.signal.find_peaks(np.abs(self.data)))
+
         self.I_peaks[0] = I
         self.locks = sp.signal.find_peaks(np.abs(self.data))
         I = np.amin(np.abs(self.locks - self.I_peaks[0]))
@@ -51,4 +54,14 @@ def tracking(self):
         self.tracked_phase[self.data_idx] = np.angle(
             self.data(self.I_peaks_filtered[self.data_idx]))
 
+        plt.figure(1)
         plt.plot(self.tracked_distance)
+        plt.title('Tracked distance')
+
+        plt.figure(2)
+        plt.plot(self.tracked_phase)
+        plt.title('Tracked phase')
+
+        plt.figure(3)
+        plt.plot(self.tracked_amplitude)
+        plt.title('Tracked amplitude')
