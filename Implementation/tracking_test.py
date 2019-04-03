5d5369da006ba59896b1eb94dadf8ab3885b5632
Emil <E.Johansson@zoho.com>
2019-04-03 14:36:06 +0200

    m

--- Implementation/tracking_test.py
+++ Implementation/tracking_test.py
@@ -226,10 +226,10 @@ class Tracking:
             self.tracked_distance[0][self.data_idx] = self.I_peaks_filtered[0][self.data_idx] / dist * interval
 
             self.tracked_amplitude[0][self.data_idx] = np.abs(
-                self.data(self.I_peaks_filtered[0][self.data_idx]))
-            self.data(self.I_peaks_filtered[0][self.data_idx])
+                self.data(int(self.I_peaks_filtered[0][self.data_idx])))
+            self.data(int(self.I_peaks_filtered[0][self.data_idx]))
             self.tracked_phase[self.data_idx] = np.angle(
-                self.data(self.I_peaks_filtered[0][self.data_idx]))
+                self.data(int(self.I_peaks_filtered[0][self.data_idx])))
 
         return self.tracked_distance
 
