888d5bea27d76628ea72535a162034e993ae6e7b
Emil <E.Johansson@zoho.com>
2019-04-03 14:55:12 +0200

    m

--- Implementation/tracking_test.py
+++ Implementation/tracking_test.py
@@ -227,7 +227,7 @@ class Tracking:
             # Emil
             print(self.I_peaks)
 
-            print(self.I_peaks_filtered[0][self.data_idx])
+            print(self.I_peaks_filtered[0][int(self.data_idx)])
             self.tracked_distance[0][self.data_idx] = self.I_peaks_filtered[0][self.data_idx] / dist * interval
 
             self.tracked_amplitude[0][self.data_idx] = np.abs(
