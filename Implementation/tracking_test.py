415eb377a70e8a8be617e0b9af91bcc5d778820e
Emil <E.Johansson@zoho.com>
2019-04-03 14:25:40 +0200

    m

--- Implementation/tracking_test.py
+++ Implementation/tracking_test.py
@@ -201,12 +201,14 @@ class Tracking:
             self.locks, _ = signal.find_peaks(np.abs(self.data))
             I = np.amin(self.locks - self.I_peaks_filtered[0][self.data_idx - 1])
             last_max = self.I_peaks_filtered[0][self.data_idx - 1]
+            print(last_max)
+            print(self.locks)
             I = np.amax([np.abs(self.data[int(I + last_max)]), np.abs(self.data[int(last_max-I)])])
-
+            print(I)
             if self.locks == None:
                 self.I_peaks[0][self.data_idx] = self.I_peaks[0][self.data_idx-1]
             else:
-                self.I_peaks[0][self.data_idx] = self.locks[I]
+                self.I_peaks[0][self.data_idx] = self.data[0][int(I)]
             if counter == 0:
                 self.i_avg_start = np.amax([1, self.data_idx - N_avg])
             else:
