f6c67caf91b210e8e87cfb94f5de7f4e911d1a4b
Emil <E.Johansson@zoho.com>
2019-04-04 16:52:33 +0200

    m

--- Implementation/tracking_test.py
+++ Implementation/tracking_test.py
@@ -142,20 +142,22 @@ class Tracking:
         else:
             self.locks, _ = signal.find_peaks(np.abs(self.data))
             # I = np.amin(self.locks - self.I_peaks_filtered[0][self.data_idx - 1]) #amin and abs?
-            I = np.amin(np.abs(self.locks - self.I_peaks_filtered[0][self.data_idx - 1]))
-            last_max = self.I_peaks[0][self.data_idx - 1]
-            print("locks: ", self.locks)
+            Index_in_locks = np.argmin(
+                np.abs(self.locks - self.I_peaks_filtered[0][self.data_idx - 1]))
+            I = self.locks[Index_in_locks]
+            #last_max = self.I_peaks[0][self.data_idx - 1]
+            #print("locks: ", self.locks)
             print("Index: ", I)
             print("Last_max: ", last_max)
-            if I + last_max >= dist or last_max - I < 0:
-                pass
-            else:
-                List_of_largest_amp = [np.abs(self.data[int(I + last_max)]),  # if close to one end the last_max and I will go out of bounds
-                                       np.abs(self.data[int(last_max-I)])]
-                if List_of_largest_amp[0] > List_of_largest_amp[1]:
-                    I = I + last_max
-                else:
-                    I = last_max - I
+            # if I + last_max >= dist or last_max - I < 0:
+            #     pass
+            # else:
+            #     List_of_largest_amp = [np.abs(self.data[int(I + last_max)]),  # if close to one end the last_max and I will go out of bounds
+            #                            np.abs(self.data[int(last_max-I)])]
+            #     if List_of_largest_amp[0] > List_of_largest_amp[1]:
+            #         I = I + last_max
+            #     else:
+            #         I = last_max - I
 
             print("Index: ", I)
             print("Distance to target: ", matlab_dist[int(I)])
