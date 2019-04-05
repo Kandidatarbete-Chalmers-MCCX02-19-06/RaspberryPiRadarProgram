b1eef6deab58efa98ea3dbc5047759a84873aa87
Emil <E.Johansson@zoho.com>
2019-04-05 14:47:13 +0200

    M

--- Implementation/tracking_test.py
+++ Implementation/tracking_test.py
@@ -245,7 +245,7 @@ class Tracking:
             self.I_peaks_filtered[0][self.data_idx] = np.round(
                 np.mean(self.I_peaks[0][self.i_avg_start:self.data_idx]))
             # print("I_peaks med kolon",
-                  self.I_peaks[0][self.i_avg_start:self.data_idx])
+                  #self.I_peaks[0][self.i_avg_start:self.data_idx])
             # print("numpoints", dist)
 
             # self.I_peaks_filtered[0][self.data_idx] = np.argmax(  # new method: just taking
