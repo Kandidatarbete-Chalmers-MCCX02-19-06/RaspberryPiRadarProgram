d8dd2cf2b580cf0f15ab84a21fe330346238dac1
Emil <E.Johansson@zoho.com>
2019-04-05 14:42:17 +0200

    M

--- Implementation/tracking_test.py
+++ Implementation/tracking_test.py
@@ -101,7 +101,7 @@ def config_setup():
     config = configs.EnvelopeServiceConfig()
     # config = configs.IQServiceConfig()
     config.range_interval = [0.4, 0.8]
-    config.sweep_rate = 10
+    config.sweep_rate = 2
     config.gain = 1
     config.session_profile = configs.EnvelopeServiceConfig.MAX_SNR
     return config
