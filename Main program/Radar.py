3146980e73762c336d76cb8e92729437fd1d977f
Erik Angert Svensson <erik.angertsvensson@gmail.com>
2019-04-05 22:16:35 +0200

    Timing

--- Main program/Radar.py
+++ Main program/Radar.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import numpy as np
+from scipy import signal
 import queue
 import copy       # import for static variable run in class
 
@@ -50,25 +51,40 @@ class Radar(threading.Thread):
         self.HR_filter_queue = HR_filter_queue
         #self.a = a
         #self.RR_filter_queue = RR_filter_queue
+
+        # Tracking method variables
+        self.N_avg = 10     # number of average values in tracking
+        self.I_peaks = np.zeros(self.N_avg)
+        self.locs = np.zeros(self.N_avg)
+        self.I_peaks_filtered = np.zeros(self.N_avg)
+        self.tracked_distance = np.zeros(self.N_avg)
+        self.tracked_amplitude = np.zeros(self.N_avg)
+        self.tracked_phase = np.zeros(self.N_avg)
+        self.threshold = 0  # variable for finding peaks above threshold
+        self.data_idx = 0
+        self.real_dist = np.linspace(self.config.range_interval[0], self.config.range_interval[1], num=self.num_points)  # converts index to real length
+        self.counter = 0  # Used only for if statement only for first iteration and not when data_idx goes back to zero
+
         super(Radar, self).__init__()  # Inherit threading vitals
+
 
     # Loop which collects data from the radar, tracks the maximum peak and filters it for further signal processing. The final filtered data is put into a queue.
     def run(self):
-
         self.client.start_streaming()  # Starts Acconeers streaming server
-        # static variable impported from bluetooth_app class (In final version)
+        # static variable imported from bluetooth_app class (In final version)
         while self.go:
             # for i in range(self.seq*2):
             self.get_data()
-            self.tracker()
-            self.filter_HeartRate()
-            self.filter_RespRate()
-            self.data_idx += 1
+            tracked_distance = self.tracking()
+            print("tracked distance: ", tracked_distance)
+            # self.filter_HeartRate()
+            # self.filter_RespRate()
+            # self.data_idx += 1
             # if self.data_idx % self.config.sweep_rate == 0:
             #    print("Still getting data")
             self.HR_filter_queue.put(2)
-            if self.data_idx >= self.seq:  # Resets matrix index to zero for filtering.
-                self.data_idx = 0
+            # if self.data_idx >= self.seq:  # Resets matrix index to zero for filtering.
+            #   self.data_idx = 0
 
         print("End of getting data from radar")
         self.client.disconnect()
@@ -103,7 +119,57 @@ class Radar(threading.Thread):
         pass
 
     # Tracks the maximum peak from collected data which is filtered for further signal processing
-    def tracker(self):
-        self.amplitude = np.abs(self.data)
-        self.peak = np.argmax(self.amplitude)
-        self.peak_vector[0][self.data_idx] = self.data[0][self.peak]
+    def tracking(self):
+        if self.data_idx == 0 and self.counter == 0:      # things that only happens first time
+            I = np.argmax(np.abs(self.data))
+            self.I_peaks[:] = I
+            self.I_peaks_filtered[0] = self.I_peaks[0]
+            self.tracked_distance[0] = self.real_dist[int(self.I_peaks_filtered[0])]
+            self.tracked_amplitude[0] = np.abs(self.data[int(self.I_peaks_filtered[0])])
+            self.tracked_phase[0] = np.angle(self.data[int(self.I_peaks_filtered[0])])
+
+        # After first seq continous tracking
+        else:
+            self.locs, _ = signal.find_peaks(np.abs(self.data))        # find local maximas in data
+            self.locs = [x for x in self.locs if(np.abs(self.data[x]) > self.threshold)]      # removes local maxima if under threshhold
+            difference = np.subtract(self.locs, self.I_peaks_filtered[self.data_idx])
+            # print("locks: ", self.locs)
+            # print("Last I_peaks_filtered: ", self.I_peaks_filtered[self.data_idx])
+            # print("difference: ", difference)
+            abs = np.abs(difference)
+            argmin = np.argmin(abs)
+            Index_in_locks = argmin     # index of closest peak in locs
+
+            # Index_in_locks = np.argmin(np.abs(self.locks - self.I_peaks_filtered[self.data_idx - 1]))       # difference between current peak index and last peak index
+
+            if len(self.locs) == 0:        # if no peak is found
+                self.I_peaks[self.data_idx] = self.I_peaks[self.data_idx - 1]
+                print("Last value. Not updated.")
+            else:
+                I = self.locs[int(Index_in_locks)]
+                self.I_peaks[self.data_idx] = I
+
+            # print("I_peaks: ", self.I_peaks)
+
+            # if self.counter == 0:  # Questions about this part.
+            #     self.i_avg_start = 0        # this will be 0 as long as counter == 0
+            #     if self.data_idx == self.N_avg - 1:  # change dist to nmbr of sequences later
+            #         self.counter = 1
+            # else:
+            # self.i_avg_start = self.data_idx - (self.N_avg - 1)
+
+            self.I_peaks_filtered[self.data_idx] = np.round(np.mean(self.I_peaks))      # mean value of N_avg latest peaks
+
+            self.threshold = np.abs(self.data[int(self.I_peaks_filtered[self.data_idx])])*0.5       # determines threshold
+            self.tracked_distance[self.data_idx] = self.real_dist[int(self.I_peaks_filtered[self.data_idx])]
+            self.tracked_amplitude[self.data_idx] = np.abs(self.data[int(self.I_peaks_filtered[self.data_idx])])
+            self.tracked_phase[self.data_idx] = np.angle(self.data[int(self.I_peaks_filtered[self.data_idx])])
+
+        # print("I_peaks_filtered: ", self.I_peaks_filtered)
+
+        self.data_idx += 1
+        if self.data_idx == self.N_avg:
+            self.data_idx = 0
+
+        return self.tracked_distance[self.data_idx - 1]
+
