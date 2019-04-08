86af4527b78f378c6fc170e88ad785343662a4cf
Emil <E.Johansson@zoho.com>
2019-04-08 14:49:35 +0200

    M

--- Main program/DataAcquisition.py
+++ Main program/DataAcquisition.py
@@ -72,7 +72,7 @@ class DataAcquisition(threading.Thread):
 
     def get_data(self):
         info, data = self.client.get_next()
-        print(data)
+        # print(data)
         print("length of data {}".format(len(data[0])))
         print("info {}".format(info))
         return data
@@ -120,7 +120,7 @@ class DataAcquisition(threading.Thread):
             plot_hist_pos = self.hist_pos - self.hist_pos.mean()
             plot_hist_pos_zoom = self.hist_pos[hist_len//2:] - self.hist_pos[hist_len//2:].mean()
 
-            iq_val = np.exp(1j*np.angle(sweep[0][com_idx])) * self.lp_ampl[0][com_idx]
+            iq_val = np.exp(1j*np.angle(sweep[0][com_idx])) * self.lp_ampl[com_idx]
 
             plot_data = {
                 "abs": self.lp_ampl,
