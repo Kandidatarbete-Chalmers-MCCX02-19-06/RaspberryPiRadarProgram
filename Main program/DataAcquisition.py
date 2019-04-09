2d5342622847da0051f3965c9f7a536d3b7f7237
Albin Warnicke <albin.warnicke@live.se>
2019-04-09 17:40:37 +0200

    Modified tracking program.

--- Main program/DataAcquisition.py
+++ Main program/DataAcquisition.py
@@ -123,11 +123,11 @@ class DataAcquisition(threading.Thread):
                     self.track_peak_index[-1] = self.track_peak_index[-2]
                 if len(self.track_peak_index) > self.number_of_averages:  # removes oldest value
                     self.track_peak_index.pop(0)
-                if data[self.track_peak_index[-1]] < 0.1 * data[max_peak]:
-                    self.track_peak_index.clear() # reset the array
-                    self.track_peak_index.append(max_peak)  # new peak as global max
-                    # self.local_peaks_index[:] = max_peak # reset the array and take the new global max as
-                    self.threshold = 0.5 * max_peak
+                # if data[self.track_peak_index[-1]] < 0.1 * data[max_peak]:
+                #     self.track_peak_index.clear() # reset the array
+                #     self.track_peak_index.append(max_peak)  # new peak as global max
+                #     # self.local_peaks_index[:] = max_peak # reset the array and take the new global max as
+                #     self.threshold = 0.5 * max_peak
 
             a = self.alpha(0.25, self.dt)
             #self.local_peaks_average_index = a * np.round(np.average(self.track_peak_index)) + (
