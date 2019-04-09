0173baefe1ac9d777854adc0c5239ac0ac714934
Albin Warnicke <albin.warnicke@live.se>
2019-04-09 14:27:17 +0200

    Modified tracking program.

--- Main program/DataAcquisition.py
+++ Main program/DataAcquisition.py
@@ -34,7 +34,7 @@ class DataAcquisition(threading.Thread):
         # Settings for radar setup
         self.config.range_interval = [0.2, 0.6]  # Measurement interval
         self.config.sweep_rate = 24  # Frequency for collecting data
-        self.config.gain = 0.7  # Gain between 0 and 1.
+        self.config.gain = 1  # Gain between 0 and 1.
 
         # self.sweep_index = 0 # för plotten
         # för plotten
