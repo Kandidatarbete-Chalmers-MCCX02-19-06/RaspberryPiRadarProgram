49eb119c99ed403340add69e8a93a6f088df4a58
Albin Warnicke <albin.warnicke@live.se>
2019-04-09 18:02:09 +0200

    Modified tracking program.

--- Main program/DataAcquisition.py
+++ Main program/DataAcquisition.py
@@ -109,21 +109,19 @@ class DataAcquisition(threading.Thread):
 
                 #print(self.local_peaks_index)
                 index = 0
-                index_list = []
-                # print("Threshold: ",self.threshold)
-                for peak in self.local_peaks_index:
-                    if np.abs(data[peak]) < self.threshold:
-                        # np.delete(self.local_peaks_index, index)
-                        index_list.append(index)
-                        index += 1
-                np.delete(self.local_peaks_index, index_list)       # deletes all indexes with amplitude < threshold
-                #self.local_peaks_index = self.local_peaks_index[(np.abs(data[:]) > self.threshold)]
+                # index_list = []
+                # # print("Threshold: ",self.threshold)
+                # for peak in self.local_peaks_index:
+                #     if np.abs(data[peak]) < self.threshold:
+                #         # np.delete(self.local_peaks_index, index)
+                #         index_list.append(index)
+                #         index += 1
+                # np.delete(self.local_peaks_index, index_list)       # deletes all indexes with amplitude < threshold
+                self.local_peaks_index = self.local_peaks_index[(np.abs(data[:]) > self.threshold)]
                 # print("local peaks: ",self.local_peaks_index)
                 # self.local_peaks_index = [x for x in self.local_peaks_index if (np.abs(data[x]) > self.threshold)]
                 # print("local peaks: ",self.local_peaks_index)
                 peak_difference_index = np.subtract(self.local_peaks_index, self.local_peaks_average_index)
-                print("difference abs: ",np.abs(peak_difference_index))
-                print("argmin: ",np.argmin(np.abs(peak_difference_index)))
                 self.track_peak_index.append(self.local_peaks_index[np.argmin(np.abs(peak_difference_index))]) # min difference of index
                 if len(self.local_peaks_index) == 0:
                     print("No local peak found")
