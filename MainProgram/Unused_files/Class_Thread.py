5b4b530290b7b8423fdd7d78c127c06abec30821
Emil <E.Johansson@zoho.com>
2019-04-11 19:31:06 +0200

    name change on main folder

---
+++ MainProgram/Unused_files/Class_Thread.py
@@ -0,0 +1,58 @@
+import time
+import threading
+import numpy as np
+import queue
+import copy
+
+from acconeer_utils.clients.reg.client import RegClient
+from acconeer_utils.clients.json.client import JSONClient
+from acconeer_utils.clients import configs
+from acconeer_utils import example_utils
+from acconeer_utils.mpl_process import PlotProcess, PlotProccessDiedException, FigureUpdater
+
+
+class Radar():
+    def __init__(self, radar_queue, interrupt_queue):
+        # Setup for collecting data from radar
+        self.args = example_utils.ExampleArgumentParser().parse_args()
+        example_utils.config_logging(self.args)
+        if self.args.socket_addr:
+            self.client = JSONClient(self.args.socket_addr)
+        else:
+            port = self.args.serial_port or example_utils.autodetect_serial_port()
+            self.client = RegClient(port)
+
+        self.client.squeeze = False
+        self.config = configs.IQServiceConfig()
+        self.config.sensor = self.args.sensors
+
+        self.config.range_interval = [0.2, 0.6]  # Measurement interval
+        self.config.sweep_rate = 1  # Frequency for collecting data
+        self.config.gain = 1  # Gain between 0 and 1.
+        self.time = 10  # Duration for a set amount of sequences
+        self.seq = self.config.sweep_rate * self.time
+
+        self.info = self.client.setup_session(self.config)  # Setup acconeer radar session
+        self.num_points = self.info["data_length"]  # Amount of data points per sampel
+
+        #### Det här kanske inte ska vara i den här klassen #####
+        # Vector for radar values from tracked data
+        self.peak_vector = np.zeros((1, self.seq), dtype=np.csingle)
+        self.data_idx = 0  # Inedex for peak vector used for filtering
+
+        self.radar_queue = radar_queue
+        self.interrupt_queue = interrupt_queue
+        self.timeout = time.time() + self.time
+        b = 0
+
+    # Loop which collects data from the radar, tracks the maximum peak and filters it for further signal processing. The final filtered data is put into a queue.
+    def get_data(self):
+        self.client.start_streaming()  # Starts Acconeers streaming server
+        while True:
+            self.info, self.data = self.client.get_next()
+            print("Getting data")
+            if self.interrupt_queue.empty() == False or time.time() >= self.timeout:  # Interrupt from bluetooth
+                # self.interrupt_queue.get()
+                print('Breaking loop')
+                break
+        self.client.disconnect()
---
+++ MainProgram/main.py
@@ -0,0 +1,66 @@
+from typing import Any, Union
+
+import time
+import threading
+import numpy as np
+import queue
+
+#import Radar
+# import bluetooth_server        # import bluetooth class
+import data_acquisition      # Import class which collects and filters relevant data.
+import signal_processing
+
+# Bluetooth imports
+#import bluetooth
+import math
+import random
+import subprocess       # for Raspberry Pi shutdown
+
+
+def main():
+    # subprocess.call(
+    #    "./Documents/evk_service_linux_armv71_xc112/utils/acc_streaming_server_rpi_xc112_r2b_xr112_r2b_a111_r2c")
+
+    radar_queue = queue.Queue()  # Not used right now?
+    HR_filtered_queue = queue.Queue()
+    HR_final_queue = queue.Queue()
+    RR_filtered_queue = queue.Queue()
+    RR_final_queue = queue.Queue()
+    RTB_final_queue = queue.Queue()  # Real time breating final queue
+    go = ["True"]
+    run_measurement = []
+    sample_freq = 0
+    list_of_variables_for_threads = {"HR_filtered_queue": HR_filtered_queue, "HR_final_queue": HR_final_queue,
+                                     "RR_filtered_queue": RR_filtered_queue, "RR_final_queue": RR_final_queue, "RTB_final_queue": RTB_final_queue, "go": go, "run_measurement": run_measurement, "sample_freq": sample_freq}
+    # heart_rate_queue = queue.Queue()
+    # resp_rate_queue = queue.Queue()
+
+    # radar = Radar.Radar(radar_queue, go)
+    # radar.start()
+    dataAcquisition = data_acquisition.DataAcquisition(list_of_variables_for_threads)
+    dataAcquisition.start()
+    signal_processings = signal_processing.SignalProcessing(list_of_variables_for_threads)
+    # signal_processing.thread_start()
+
+    # bluetooth_server = bluetooth_server.BluetoothServer(list_of_variables_for_threads)
+    # bluetooth_server.app_data()
+    # print('End of bluetooth_app')
+    time.sleep(300)
+    # interrupt_queue.put(1)
+    list_of_variables_for_threads["go"] = go.pop(0)
+    # radar.join()
+    # signal_processings.heart_rate_thread.join()
+    signal_processings.schmittTrigger_thread.join()
+    print("signal_processing is closed")
+    time.sleep(1 / 20)  # Making sure signal processing have data in queue before radar quits.
+    dataAcquisition.join()
+    print("radar is closed")
+    # bluetooth_server.connect_device_thread.join()
+    #print("connect_device is closed")
+
+    print('Shut down succeed')
+    #subprocess.call(["sudo", "shutdown", "-r", "now"])
+
+
+if __name__ == "__main__":
+    main()
