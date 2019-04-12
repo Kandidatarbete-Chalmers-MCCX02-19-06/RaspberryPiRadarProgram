55bb281911d69d7fc88354e8d13aba826b4a4416
Emil <E.Johansson@zoho.com>
2019-04-12 17:04:53 +0200

    M

--- MainProgram/bluetooth_server.py
+++ MainProgram/bluetooth_server.py
@@ -69,13 +69,13 @@ class BluetoothServer:
             pass
 
     def real_time_breating_to_app(self):
-        # try:
-        # TEMP: Takes data from filtered resp.rate
-        real_time_breating_to_app = self.RTB_final_queue.get()
-        #print("Real time breathing to app {}".format(real_time_breating_to_app))
-        self.write_data_to_app(real_time_breating_to_app, 'real time breath')
-        # except:
-        #    pass
+        try:
+            # TEMP: Takes data from filtered resp.rate
+            real_time_breating_to_app = self.RTB_final_queue.get_nowait()
+            #print("Real time breathing to app {}".format(real_time_breating_to_app))
+            self.write_data_to_app(real_time_breating_to_app, 'real time breath')
+        except:
+            pass
 
     def connect_device(self):
         os.system("echo 'power on\nquit' | bluetoothctl")  # Startup for bluetooth on rpi
