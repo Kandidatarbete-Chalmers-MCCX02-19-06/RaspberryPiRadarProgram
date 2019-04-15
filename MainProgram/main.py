069f1d067027f40f3462790ef34659cf72dac9b8
Albin Warnicke <albin.warnicke@live.se>
2019-04-15 12:04:20 +0200

    Modified tracking program.

--- MainProgram/main.py
+++ MainProgram/main.py
@@ -29,7 +29,7 @@ def main():
     #print(p.read()) # fungerar
 
     process = subprocess.Popen(
-        "lxterminal -e ./home/pi/Documents/evk_service_linux_armv71_xc112/utils/acc_streaming_server_rpi_xc112_r2b_xr112_r2b_a111_r2c",
+        'lxterminal -e "./home/pi/Documents/evk_service_linux_armv71_xc112/utils/acc_streaming_server_rpi_xc112_r2b_xr112_r2b_a111_r2c"',
         stdout=subprocess.PIPE,
         stderr=None,
         shell=True
