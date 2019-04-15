709bdb5da5ed6ae3562b2163843891496f26b260
Emil <E.Johansson@zoho.com>
2019-04-15 14:27:35 +0200

    M

--- MainProgram/signal_processing.py
+++ MainProgram/signal_processing.py
@@ -120,11 +120,11 @@ class SignalProcessing:
             # self.RTB_final_queue.put(trackedRRvector[countHys - 1])
 
             if countHys == self.sample_freq * Tc:
-                # Hcut = np.sqrt(np.mean(np.square(trackedRRvector)))/2  # rms of trackedRRvector
-                Hcut = 0.2
+                Hcut = np.sqrt(np.mean(np.square(trackedRRvector)))/2  # rms of trackedRRvector
+                #Hcut = 0.2
                 Lcut = -Hcut
-                print("Hcut: ", Hcut)       # se vad hysteres blir
-                print("The last value of vector {}".format(trackedRRvector[countHys-1]))
+                # print("Hcut: ", Hcut)       # se vad hysteres blir
+                # print("The last value of vector {}".format(trackedRRvector[countHys-1]))
                 # TODO Hinder så att insvängningstiden för filtret hanteras
                 countHys = 0
 
