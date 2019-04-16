c614c26fdeef5e2d4940b423a9136c61cedb2f41
Albin Warnicke <albin.warnicke@live.se>
2019-04-16 18:13:42 +0200

    Modified tracking program.

--- MainProgram/data_acquisition.py
+++ MainProgram/data_acquisition.py
@@ -43,7 +43,7 @@ class DataAcquisition(threading.Thread):
         # Settings for radar setup
         self.config.range_interval = [0.4, 1.4]  # Measurement interval
         # Frequency for collecting data. To low means that fast movements can't be tracked.
-        self.config.sweep_rate = 60
+        self.config.sweep_rate = 50
         # For use of sample freq in other threads and classes.
         self.list_of_variables_for_threads["sample_freq"] = self.config.sweep_rate
         # The hardware of UART/SPI limits the sweep rate.
