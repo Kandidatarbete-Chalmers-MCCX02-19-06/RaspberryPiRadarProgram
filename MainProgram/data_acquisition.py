6e6a0823a717ef30540cae5eda901272406193d7
Albin Warnicke <albin.warnicke@live.se>
2019-04-17 18:42:20 +0200

    Modified tracking program.

--- MainProgram/data_acquisition.py
+++ MainProgram/data_acquisition.py
@@ -174,10 +174,9 @@ class DataAcquisition(threading.Thread):
             info, data = self.client.get_next()
         else:
             info, data = self.client.get_next()
-        if info[-1]['sequence_number'] > self.run_times + 50:
-            print("sequence diff over 50")
-            for i in range(0,50):
-                print(i)
+        if info[-1]['sequence_number'] > self.run_times + 20:
+            print("sequence diff over 20")
+            for i in range(0,20):
                 self.client.get_next()
             info, data = self.client.get_next()
             self.run_times = info[-1]['sequence_number']
