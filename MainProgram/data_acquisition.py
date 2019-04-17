4e9851df6ee72ca32f0473e71dba78b015179d41
Albin Warnicke <albin.warnicke@live.se>
2019-04-17 17:07:19 +0200

    Modified tracking program.

--- MainProgram/data_acquisition.py
+++ MainProgram/data_acquisition.py
@@ -144,8 +144,8 @@ class DataAcquisition(threading.Thread):
 
                 # Send to app
                 #start = time.time()
-                self.bluetooth_server.write_data_to_app(tracked_data["relative distance"], 'real time breath')
-                #self.bluetooth_server.write_data_to_app(bandpass_filtered_data_RR, 'real time breath')
+                #self.bluetooth_server.write_data_to_app(tracked_data["relative distance"], 'real time breath')
+                self.bluetooth_server.write_data_to_app(bandpass_filtered_data_RR, 'real time breath')
                 #done = time.time()
                 #print('send to app', (done - start)*1000)
             try:
