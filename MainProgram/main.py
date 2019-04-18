3b837b5d91c81c3bf94719f36acfb556b7ada37a
Erik Angert Svensson <erik.angertsvensson@gmail.com>
2019-04-18 19:19:40 +0200

    Plot of fft

--- MainProgram/main.py
+++ MainProgram/main.py
@@ -67,12 +67,12 @@ def main():
     # SignalProcessing object used below
     signal_processing = signal_processing_module.SignalProcessing(list_of_variables_for_threads, bluetooth_server, FFTfreq, FFTamplitude)
 
+    plt.plot(FFTfreq, FFTamplitude)
+    plt.show()
     while list_of_variables_for_threads.get('go'):      # Lets threads and thereby program run while go is True. Go is set from app
         # Test of FFT, remove later
-        plt.clf()
         FFTfreq, FFTamplitude = signal_processing.getFFTvalues()
-        plt.plot(FFTfreq, FFTamplitude)
-        plt.show()
+        plt.draw()
         time.sleep(1)
 
     # Waits for running threads to finish their loops
