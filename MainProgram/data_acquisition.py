0ec91ed11dbd9c920b944ad9a113fdc1fea54e49
Albin Warnicke <albin.warnicke@live.se>
2019-04-18 09:56:46 +0200

    Modified tracking program.

--- MainProgram/data_acquisition.py
+++ MainProgram/data_acquisition.py
@@ -116,6 +116,7 @@ class DataAcquisition(threading.Thread):
         self.client.start_streaming()  # Starts Acconeers streaming server
         # runtimeold=time.time()
         while self.go:
+            print('go',self.go)
             self.run_times = self.run_times + 1
             #startstart = time.time()
             #runtime = time.time()
