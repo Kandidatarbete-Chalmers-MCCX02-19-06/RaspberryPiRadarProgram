25902db841077bd564e2d8a5dcaf6933e3192580
Emil <E.Johansson@zoho.com>
2019-04-25 18:41:46 +0200

    m

--- MainProgram/signal_processing_module.py
+++ MainProgram/signal_processing_module.py
@@ -82,7 +82,7 @@ class SignalProcessing:
             self.FFT_old_values[index_in_FFT_old_values][:] = fft_signal_out_dB
             # RBW = self.freq[1] - self.freq[0] # Used where?
             print("This new FFT: ", fft_signal_out_dB[2])
-            saved_old = self.FFT_old_values[:][2]
+            saved_old = self.FFT_old_values[2][:]
             print("length: ", len(saved_old))
             print("Saved old FFT: ", self.FFT_old_values)
             #print("Rows", len(self.FFT_old_values))
