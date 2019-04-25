b83b2e420d99812ffc5b87eaa5a9dcd90036591f
Emil <E.Johansson@zoho.com>
2019-04-25 10:26:41 +0200

    M

--- MainProgram/signal_processing_module.py
+++ MainProgram/signal_processing_module.py
@@ -89,7 +89,7 @@ class SignalProcessing:
             next_peak = []
             for i in Peaks:
                 next_peak.append(FFT_in_interval[i])
-                print("Next peak", next_peak)
+            print("Next peak", next_peak)
 
             #FFT_averaged = [x for x in FFT_averaged if (x < FHighRR and x > FLowRR)]
 
