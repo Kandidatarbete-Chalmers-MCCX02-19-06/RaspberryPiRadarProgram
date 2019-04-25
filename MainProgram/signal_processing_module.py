7f94c1cef44cf1c15e84e34dcc4adb35dfb6fcfa
Emil <E.Johansson@zoho.com>
2019-04-25 18:21:23 +0200

    m

--- MainProgram/signal_processing_module.py
+++ MainProgram/signal_processing_module.py
@@ -83,7 +83,9 @@ class SignalProcessing:
             # RBW = self.freq[1] - self.freq[0] # Used where?
             print("This new FFT: ", fft_signal_out_dB[2])
 
-            print("Saved old FFT: ", self.FFT_old_values[2][:])
+            #print("Saved old FFT: ", self.FFT_old_values[2][:])
+            print("Rows", len(self.FFT_old_values))
+            print("Columns", len(self.FFT_old_values[0]))
             # fft movemean
             FFT_averaged = self.mean_of_old_values(FFT_counter)
             print("Averaged FFT: ", FFT_averaged[:][2])
