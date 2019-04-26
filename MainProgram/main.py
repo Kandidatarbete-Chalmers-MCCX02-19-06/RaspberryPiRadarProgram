b0f99db22720d2de7d0b63447d0883af3cde95cf
Emil <E.Johansson@zoho.com>
2019-04-26 16:25:58 +0200

    m

--- MainProgram/main.py
+++ MainProgram/main.py
@@ -76,7 +76,7 @@ def main():
 
     plt.plot(FFTfreq, FFTamplitude)
     plt.pause(1)
-    plt.xlim(1, 3)
+    plt.xlim(1*60, 3*60)
     # Lets threads and thereby program run while go is True. Go is set from app
     while list_of_variables_for_threads.get('go'):
         # Test of FFT, remove later
