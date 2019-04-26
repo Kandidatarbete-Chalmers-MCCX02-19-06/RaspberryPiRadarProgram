72c1f7493fdafccdeeb2b4348e8d9c1965b270e1
Albin Warnicke <albin.warnicke@live.se>
2019-04-26 14:48:40 +0200

    Try a new noise canceling method.

--- MainProgram/data_acquisition_module.py
+++ MainProgram/data_acquisition_module.py
@@ -51,7 +51,7 @@ class DataAcquisition(threading.Thread):
         # Settings for radar setup
         self.config.range_interval = [0.6, 1]  # Measurement interval
         # Frequency for collecting data. To low means that fast movements can't be tracked.
-        self.config.sweep_rate = 40  # Probably 40 is the best without graph
+        self.config.sweep_rate = 20  # Probably 40 is the best without graph
         # For use of sample freq in other threads and classes.
         self.list_of_variables_for_threads["sample_freq"] = self.config.sweep_rate
         # The hardware of UART/SPI limits the sweep rate.
@@ -185,7 +185,7 @@ class DataAcquisition(threading.Thread):
     def tracking(self, data):
         data = np.array(data).flatten()
         data_length = len(data)
-        print(data_length)
+        #print(data_length)
         amplitude = np.abs(data)
         power = amplitude * amplitude
 
