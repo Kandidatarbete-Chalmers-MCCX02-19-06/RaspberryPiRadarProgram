077186f73e32e5badb6dffddaf43f35d50ba2fc9
Albin Warnicke <albin.warnicke@live.se>
2019-04-29 15:45:47 +0200

    Improving pulse detection

--- MainProgram/data_acquisition_module.py
+++ MainProgram/data_acquisition_module.py
@@ -296,11 +296,7 @@ class DataAcquisition(threading.Thread):
             #    (wrapped_phase - self.last_phase) / (4 * np.pi)
 
             # TODO testa med konjugat
-            com_idx = int(self.track_peak_relative_position * data_length)
-            print('com_idx',com_idx) samma
-            print('average index',self.track_peaks_average_index)
-            com_idx=self.track_peaks_average_index
-            delta_angle = np.angle(data[com_idx] * np.conj(self.last_data[com_idx]))
+            delta_angle = np.angle(data[self.track_peaks_average_index] * np.conj(self.last_data[self.track_peaks_average_index]))
             vel = self.list_of_variables_for_threads["sample_freq"] * 2.5 * delta_angle / (2 * np.pi)
             self.low_pass_vel = self.low_pass_const * vel + \
                 (1 - self.low_pass_const) * self.low_pass_vel
