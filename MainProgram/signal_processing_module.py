ec7d7896673f99e4aa2e1f019f7b7a24091eec4f
Albin Warnicke <albin.warnicke@live.se>
2019-05-02 16:49:08 +0200

    Improving pulse detection

--- MainProgram/signal_processing_module.py
+++ MainProgram/signal_processing_module.py
@@ -89,7 +89,7 @@ class SignalProcessing:
             #print(fft_signal_out)
             fft_signal_out_dB = 20*np.log10(fft_signal_out)
             print(type(fft_signal_out_dB))
-            print(type(self.FFT_old_values))
+            print(type(self.FFT_old_values[index_in_FFT_old_values]))
             self.FFT_old_values[index_in_FFT_old_values][:] = fft_signal_out_dB
 
             # RBW = self.freq[1] - self.freq[0] # Used where?
