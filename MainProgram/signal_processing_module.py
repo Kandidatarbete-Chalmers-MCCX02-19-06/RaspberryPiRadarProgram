21a7c5743c1e5cdfdb56844bc9323f47087870c9
Albin Warnicke <albin.warnicke@live.se>
2019-05-06 11:55:17 +0200

    Improving pulse detection

--- MainProgram/signal_processing_module.py
+++ MainProgram/signal_processing_module.py
@@ -141,9 +141,9 @@ class SignalProcessing:
 
                     # Determine the reliability of the found peak, if it's really the heart rate or just noise.
                     # Compares to the next largest m´peak amplitude
-                    print(np.delete(range(len(self.peak_amplitude)),found_peak_index))
+                    #print(np.delete(range(len(self.peak_amplitude)),found_peak_index))
                     next_largest_peak_amplitude = np.max(
-                        self.peak_amplitude[np.delete(range(len(self.peak_amplitude)),found_peak_index)])
+                        self.peak_amplitude[np.array(np.delete(range(len(self.peak_amplitude)),found_peak_index))])
                             #self.peak_amplitude[x for i, x in self.peak_amplitude if i != found_peak_index])
                     #next_largest_peak_amplitude = np.max(self.peak_amplitude[0:found_peak_index-1]+self.peak_amplitude[found_peak_index+1:-1])
                     #next_largest_peak_amplitude = np.max(self.peak_amplitude[np.arange(len(self.peak_amplitude)) != 3])
