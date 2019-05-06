abcc8e58631a8a1d9f59b0463b68d63424968149
Albin Warnicke <albin.warnicke@live.se>
2019-05-06 12:17:25 +0200

    Improving pulse detection

--- MainProgram/signal_processing_module.py
+++ MainProgram/signal_processing_module.py
@@ -143,8 +143,7 @@ class SignalProcessing:
                     # Compares to the next largest m´peak amplitude
                     #print(np.delete(range(len(self.peak_amplitude)),found_peak_index))
                     large_peak_amplitude = np.delete(self.peak_amplitude,found_heart_freq_amplitude_old)
-                    next_largest_peak_amplitude = np.max(
-                        self.peak_amplitude[large_peak_amplitude])
+                    next_largest_peak_amplitude = np.max(large_peak_amplitude)
                         #self.peak_amplitude[list(np.delete(range(len(self.peak_amplitude)),found_peak_index))])
                             #self.peak_amplitude[x for i, x in self.peak_amplitude if i != found_peak_index])
                     #next_largest_peak_amplitude = np.max(self.peak_amplitude[0:found_peak_index-1]+self.peak_amplitude[found_peak_index+1:-1])
