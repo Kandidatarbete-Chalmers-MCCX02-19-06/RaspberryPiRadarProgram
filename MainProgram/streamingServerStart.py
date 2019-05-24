7385314f25226df759f97fffcf30fca3c2864e25
Erik Angert Svensson <erik.angertsvensson@gmail.com>
2019-05-24 16:19:54 +0200

    Separate file for start of streamingServer. streamingServerStart.py

--- MainProgram/streamingServerStart.py
+++ MainProgram/streamingServerStart.py
@@ -2,4 +2,4 @@ import time
 import subprocess
 
 time.sleep(5)
-subprocess.call(["lxterminal", "-e", "/home/pi/Documents/evk_service_linux_armv71_xc112/utils/acc_streaming_server_rpi_xc112_r2b_xr112_r2b_a111_r2c"])
+subprocess.call(["lxterminal", "-e", "/home/pi/Documents/evk_service_linux_armv7l_xc112/utils/acc_streaming_server_rpi_xc112_r2b_xr112_r2b_a111_r2c"])
