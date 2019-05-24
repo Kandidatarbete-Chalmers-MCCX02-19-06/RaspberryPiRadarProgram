c9a2b4f77882dcae9b7a1e565e33ddb6f626b366
Erik Angert Svensson <erik.angertsvensson@gmail.com>
2019-05-24 15:58:33 +0200

    Start of Acconeer streaming server in main program

--- MainProgram/main.py
+++ MainProgram/main.py
@@ -19,7 +19,7 @@ import signal_processing_module
 def main():
     # Start of Acconeer streaming server
     time.sleep(5)
-    os.system("lxterminal -e /home/pi/Documents/evk_service_linux_armv71_xc112/utils/acc_streaming_server_rpi_xc112_r2b_xr112_r2b_a111_r2c")
+    subprocess.call("lxterminal -e /home/pi/Documents/evk_service_linux_armv71_xc112/utils/acc_streaming_server_rpi_xc112_r2b_xr112_r2b_a111_r2c")
     time.sleep(5)
 
     # Queues used for accessing data from different threads
